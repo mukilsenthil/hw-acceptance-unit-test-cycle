2699f7e59fca2ad9cd2f5d7306f1e0067a3718af
philip ritchey <pcr@tamu.edu>
2024-07-24 14:41:41 -0500

    update gemfile and specs

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -8,48 +8,55 @@ RSpec.describe MoviesController, type: :controller do
                   rating: "PG",
                   release_date: "2014-11-07",
                   director: "Don Hall, Chris Williams")
-    
+
     # TODO(student): add more movies to use for testing
   end
-  
+
   describe "when trying to find movies by the same director" do
     it "returns a valid collection when a valid director is present"
       # TODO(student): implement this test
-    
+
     it "redirects to index with a warning when no director is present"
       # TODO(student): implement this test
   end
-  
+
   describe "creates" do
     it "movies with valid parameters" do
-      get :create, params: {movie: {title: "Toucan Play This Game", director: "Armando Fox",
-                    rating: "G", release_date: "2017-07-20"}}
+      get :create, params: {movie: {title: "Threenagers", director: "The Prof.s Ritchey",
+                    rating: "G", release_date: "2015-01-20"}}
       expect(response).to redirect_to movies_path
-      expect(flash[:notice]).to match(/Toucan Play This Game was successfully created./)
-      Movie.find_by(title: "Toucan Play This Game").destroy
+      expect(flash[:notice]).to match(/Threenagers was successfully created./)
+      Movie.find_by(title: "Threenagers").destroy
     end
   end
-  
+
   describe 'updates' do
     it "redirects to the movie details page and flashes a notice" do
-      movie = Movie.create(title: 'Outfoxed!', director: 'Nick Mecklenburg',
-                           rating: 'PG-13', release_date: '2023-12-17')
-      get :update, params: { id: movie.id, movie: { description: 'Critics rave about this epic new thriller. Watch as main characters Armando Fox ' \
-                                                                 'and Michael Ball, alongside their team of TAs, battle against the challenges of ' \
-                                                                 'a COVID-19-induced virtual semester, a labyrinthian and disconnected assignment ' \
-                                                                 'stack, and the ultimate betrayal from their once-trusted ally: Codio exams.' } }
+      movie = Movie.create(title: 'Attack of the Munchkins', director: 'Sofia Grace',
+                           rating: 'PG', release_date: '2024-08-19')
+      get :update, params: {
+        id: movie.id,
+        movie: {
+          description: 'A whimsical and adventurous film where a small town is unexpectedly '\
+          'overrun by mischievous, pint-sized creatures known as Munchkins. As the townspeople ' \
+          'scramble to defend their homes and livelihoods, they uncover the Munchkins\' origins ' \
+          'and intentions. With humor and heart, the community bands together, discovering ' \
+          'hidden strengths and forging new friendships along the way. The movie blends comedy, ' \
+          'action, and a touch of fantasy, offering a light-hearted yet thrilling experience ' \
+          'for audiences of all ages.'}
+        }
 
       expect(response).to redirect_to movie_path(movie)
-      expect(flash[:notice]).to match(/Outfoxed! was successfully updated./)
+      expect(flash[:notice]).to match(/Attack of the Munchkins was successfully updated./)
       movie.destroy
     end
 
     it "actually does the update" do
-      movie = Movie.create(title: 'Outfoxed!', director: 'Nick Mecklenburg',
-                           rating: 'PG-13', release_date: '2023-12-17')
-      get :update, params: { id: movie.id, movie: { director: 'Not Nick!' } }
+      movie = Movie.create(title: 'Seven Samurai', director: 'Yasujiro Ozu',
+                           rating: 'PG', release_date: '1954-04-26')
+      get :update, params: { id: movie.id, movie: { director: 'Akira Kurosawa' } }
 
-      expect(assigns(:movie).director).to eq('Not Nick!')
+      expect(assigns(:movie).director).to eq('Akira Kurosawa')
       movie.destroy
     end
   end
